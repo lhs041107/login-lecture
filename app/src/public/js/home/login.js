c9ce7ed3fb5c4502e36bffcfbd482b90b1b952a4
lhs041107 <kdiseu017@gmail.com>
2021-10-20 16:52:43 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,5 +10,13 @@ function login() {
     const req = {
         id: id.value,
         psword: psword.value,
-    }
+    };
+    
+    fetch("/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req),
+    })
 }
